66d0d6babc2917ab894c9e211364bd05d6dcb562
ARIJSAYEED <arijsayeed@gmail.com>
2026-04-27 16:45:12 +0600

    completed user data insertation to the database

--- index.js
+++ index.js
@@ -27,6 +27,17 @@ async function runStableAPIConnect() {
         // Connect the client to the server (optional starting in v4.7)
         await client.connect();
 
+        const db = client.db("eTutionBD");
+        const usersCollection = db.collection("usersCollection");
+
+        // users-related api
+        app.post('/users', async (req, res) => {
+            const user = req.body;
+            console.log(user);
+            const result = await usersCollection.insertOne(user);
+            res.send(result)
+        })
+
         // Send a ping to confirm a successful connection
         const result = await client.db('admin').command({ ping: 1 });
         console.log(
