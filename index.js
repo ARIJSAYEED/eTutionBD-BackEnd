83f110f6b0fc71d1d60966e654eb2a018476c5a9
ARIJSAYEED <arijsayeed@gmail.com>
2026-05-13 11:27:06 +0600

    connected to db from new windows11

--- index.js
+++ index.js
@@ -1,57 +1,46 @@
-const express = require('express');
+const express = require('express')
 const cors = require('cors');
-require('dotenv').config()
-const app = express();
-const port = process.env.PORT || 3000
+const app = express()
+const port = 3000
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
-
-// middlewares 
-app.use(express.json())
+app.use(express.json());
 app.use(cors())
 
-console.log(process.env.DB_USERNAME, process.env.DB_PASSWORD);
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
 
-// Replace the placeholder with your Atlas connection string
-const uri = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.taazt4c.mongodb.net/?appName=Cluster0`;
+// mongodb connection
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+// const uri = `mongodb+srv://eTutionBD:s3Oln0LNdQKeSgte@cluster0.taazt4c.mongodb.net/?appName=Cluster0`;
+
+const uri = "mongodb://eTutionBD:s3Oln0LNdQKeSgte@ac-hcxuuhk-shard-00-00.taazt4c.mongodb.net:27017,ac-hcxuuhk-shard-00-01.taazt4c.mongodb.net:27017,ac-hcxuuhk-shard-00-02.taazt4c.mongodb.net:27017/?ssl=true&replicaSet=atlas-qgx1um-shard-0&authSource=admin&appName=Cluster0";
+
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
     serverApi: {
         version: ServerApiVersion.v1,
         strict: true,
         deprecationErrors: true,
-    },
+    }
 });
-async function runStableAPIConnect() {
+
+async function run() {
     try {
-        // Connect the client to the server (optional starting in v4.7)
+        // Connect the client to the server	(optional starting in v4.7)
         await client.connect();
-
-        const db = client.db("eTutionBD");
-        const usersCollection = db.collection("usersCollection");
-
-        // users-related api
-        app.post('/users', async (req, res) => {
-            const user = req.body;
-            console.log(user);
-            const result = await usersCollection.insertOne(user);
-            res.send(result)
-        })
-
         // Send a ping to confirm a successful connection
-        const result = await client.db('admin').command({ ping: 1 });
-        console.log(
-            'Pinged your deployment. You successfully connected to MongoDB!'
-        );
-        return result;
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
     } finally {
         // Ensures that the client will close when you finish/error
         // await client.close();
     }
 }
-runStableAPIConnect().catch(console.dir);
+run().catch(console.dir);
 
 
 app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`);
+    console.log(`Example app listening on port ${port}`)
 })
